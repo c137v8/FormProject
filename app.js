bf2a0f753652a8af197416527ca136161cfdfaf1
The rage <147223475+c137v8@users.noreply.github.com>
2025-07-07 22:10:48 +0530

    hmmm

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.get('/', (req, res) => {
 
 app.post('/submit', (req, res) => {
   console.log('Form data:', req.body);
-  res.send('<h2 style="text-align:center;">Form submitted successfully!</h2>');
+  res.send('<h2 style="text-align:center;">Data sent</h2>');
 });
 
 app.listen(PORT, () => {
